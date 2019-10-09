15f301c87d3bb3a5db83ed7c4e42ae7a178be866
landt52 <36035899+landt52@users.noreply.github.com>
2019-10-09 20:23:07 +0700

    Updates

---
+++ precache-manifest.285eef25665714b88bc00a14c2faa239.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb934a718623b13366868f8ae010f6bc",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "f6e7a8735d33f4ae399d",
+    "url": "/todoapp/static/css/main.cd300eea.chunk.css"
+  },
+  {
+    "revision": "caf4a3522be477dc86de",
+    "url": "/todoapp/static/js/2.4932af5f.chunk.js"
+  },
+  {
+    "revision": "f6e7a8735d33f4ae399d",
+    "url": "/todoapp/static/js/main.0e9b6eba.chunk.js"
+  },
+  {
+    "revision": "35dc6bdf3fdf3cd891f6",
+    "url": "/todoapp/static/js/runtime-main.f039fef0.js"
+  }
+]);
